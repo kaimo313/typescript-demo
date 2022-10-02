ae46f0fe2fd0dee037e7e6fda5eaf585bfb89cc0
kaimo <kaimo313@foxmail.com>
2022-10-03 01:03:30 +0800

    feat：TS的类型声明

---
+++ demo/02.basis.js
@@ -0,0 +1,27 @@
+// 声明一个变量a，同时指定它的类型为number
+var a;
+// a的类型设置为了number, 在以后的使用过程中a的值只能是数字
+a = 10;
+a = 33;
+a = 'hello'; // 此行代码会报错，因为变量a的类型是number, 不能赋值字符明
+var b;
+b = 'hello';
+b = 123; // 不能将类型“number”分配给类型“string”。
+// 声明完变量直接进行赋值
+// let c: boolean = false;
+// 如果变量的声明和赋值是同时进行的，TS 可以自动对变量进行类型检测
+var c = false;
+c = true;
+c = '111'; // 不能将类型“string”分配给类型“boolean”。
+// JS 中的两数是不考虑参数的类型和个数的
+// function sum(a, b){
+//     return a + b;
+// }
+// console.log(sum(123, 456)); // 579
+// console. log(sum(123, "456")); // "123456"
+// 参数以及返回值需要时数字
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(123, 456)); // 579
+console.log(sum(123, "456")); // "123456" 类型“string”的参数不能赋给类型“number”的参数。
