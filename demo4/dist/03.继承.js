4dd910bde0cef1f214ec5801e140d4c9a2459ad1
kaimo <kaimo313@foxmail.com>
2022-10-05 05:59:04 +0800

    feat：继承简介

---
+++ demo4/dist/03.继承.js
@@ -0,0 +1,32 @@
+"use strict";
+(function () {
+    // 父类
+    class Animal {
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+        }
+        sayHello() {
+            console.log("动物叫~");
+        }
+    }
+    // 使Dog类继承Animal类
+    class Dog extends Animal {
+        run() {
+            console.log(`${this.name}在跑`);
+        }
+        sayHello() {
+            console.log(`${this.name}汪汪汪`);
+        }
+    }
+    // 使Cat类继承Animal类
+    class Cat extends Animal {
+    }
+    const dog = new Dog("小黄", 2);
+    const cat = new Cat("小橘", 1);
+    console.log(dog);
+    dog.sayHello();
+    dog.run();
+    console.log(cat);
+    cat.sayHello();
+})();
